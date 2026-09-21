eb029360b20e5e379ab76bb3553b4440a8b7ef64
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-09-21 22:35:18 +0530

    router setup

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@
 
 const express=require("express");
 
+//using auth routes
+const authRouter=require("./Routes/auth.routes")
+
 //this app constant will have access to all express methods
 const app=express();
+
+//every api requires whose endpoint contains: /api/auth will be redirected to authRouter
+app.use("/api/auth",authRouter)
 
 module.exports=app
