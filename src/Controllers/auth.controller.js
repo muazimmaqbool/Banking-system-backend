8418d8a4c723afa4ce4cf4cb323fcbc312691178
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-09-21 22:53:22 +0530

    user controller

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -2,7 +2,27 @@ const userModel=require("../Models/user.model")
 
 //user register controller
 //will be used with this api: /api/auth/register inside app.routes.js file
-function userRegisterController(req,res){
-    const{email,password,name}=req.body()
+async function userRegisterController(req,res){
+    const{email,password,name}=req.body();
+
+    //checking whether email already exists or not
+    const isExists=await userModel.findOne({
+        email:email
+    })
+
+    //returing if email already exists
+    if(isExists){
+        return res.status(422).json({
+            message:"Email already exists",
+            status:"Failed"
+        })
+    }
+
+    //if user is new/email is new
+    const user=await userModel.create({
+        email,password,name
+    })
+
+    //returning with jwt token
 }
 module.exports={userRegisterController};
