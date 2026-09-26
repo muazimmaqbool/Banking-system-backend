7f8e63b398f6f470bf29b140a72b927cc5d94f3f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-09-26 12:16:35 +0530

    added send registration email function

--- src/services/email.js
+++ src/services/email.js
@@ -2,15 +2,15 @@
 
 //Set Up Nodemailer with OAuth2
 
-require('dotenv').config();
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
 //the job of this transporter use to interact with the SMTP servers of google
 //and to contact with those servers we need these things: client_id, client_secret, refresh_token
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.EMAIL_USER,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -21,12 +21,11 @@ const transporter = nodemailer.createTransport({
 // Verify the connection configuration
 transporter.verify((error, success) => {
   if (error) {
-    console.error('Error connecting to email server:', error);
+    console.error("Error connecting to email server:", error);
   } else {
-    console.log('Email server is ready to send messages');
+    console.log("Email server is ready to send messages");
   }
 });
-
 
 // Function to send email
 const sendEmail = async (to, subject, text, html) => {
@@ -39,11 +38,20 @@ const sendEmail = async (to, subject, text, html) => {
       html, // html body
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
   }
 };
 
-module.exports = sendEmail;
+async function sendRegistrationEmail(name, email) {
+  const subject = "Welcome to Backend Ledger!";
+  const text = `Hello ${name},\n\nThank you for registering at Backend Ledger. We're excited to have you on board!\n\nBest regards,\nThe Backend Ledger Team`;
+  const html = `<p>Hello ${name},</p><p>Thank you for registering at Backend Ledger. We're excited to have you on board!</p><p>Best regards,<br>The Backend Ledger Team</p>`;
+
+  await sendEmail(email, subject, text, html);
+}
+
+//used inside authController
+module.exports = {sendRegistrationEmail};
