4917e22f2cd0c7c464bd7b7b195b30c565a78e3a
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-09-26 16:57:55 +0530

    account route setup

--- src/Routes/account.routes.js
+++ src/Routes/account.routes.js
@@ -1,4 +1,13 @@
 const express=require("express");
+const {jwtAuthMiddleware}=require("../middleware/jwt")
+
 const router=express.Router();
+
+/**
+ * - POST api/accounts/
+ * - Create a new account
+ * - Protected Route
+ */
+router.post("/",jwtAuthMiddleware,)
 
 module.exports=router
